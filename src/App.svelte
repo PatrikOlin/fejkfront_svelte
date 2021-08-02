c90d1818d5f6bcdd04b57af5efe56684bbc1bb40
Patrik Olin <enmejl@gmail.com>
2021-08-02 14:03:33 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+ 	import Card from './components/card/Card.svelte'
+ 	import { onMount } from 'svelte'
+
+	export let pageTitle: string;
+    let items = [];
+
+ 	onMount(async () => {
+		const res = await fetch('https://api.fejk.company/v1/people?amount=2')
+		items = await res.json()
+		console.log(items)
+	})
+</script>
+
+<main>
+	<h1>{pageTitle}</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#each items as child}
+	<Card props={child} />
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/card/Card.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    export let props: any;
+</script>
+
+<main>
+    <h1>Här vare kort</h1>
+    <ul>
+	    <li>
+		    <p>{props.name}</p>
+	    </li>
+	    <li>
+		    <p>{props.email}</p>
+	    </li>
+	    <li>
+		    <p>{props.image}</p>
+	    </li>
+	    <li>
+		    <p>{props.address}</p>
+	    </li>
+	    <li>
+		    <p>{props.phone}</p>
+	    </li>
+	    <li>
+		    <p>{props.pin}</p>
+	    </li>
+    </ul>
+</main>
+
+<style>
+	main {
+		text-align: left;
+		padding: 1em;
+		max-width: 350px;
+		margin: 0 auto;
+        background-color: coral;
+        border-radius: 5%;
+	}
+</style>
